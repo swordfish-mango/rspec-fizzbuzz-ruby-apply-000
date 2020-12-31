a14b32ac3777e4526808f2eeba513ac37e745aca
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2020-12-31 18:56:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    return "fizzbuzz"
+    return "Fizzbuzz"
   elsif num % 3 == 0 
-    return "fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    return "buzz"
+    return "Buzz"
   else
     return "nil"
   end
